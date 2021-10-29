dd908762211272d3b2926a7710f44437aa000e8c
BarbonFisso <barbieri.mat@hotmail.it>
2021-10-29 14:50:13 +0200

    terminato es parola palindroma

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -1,3 +1,32 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma.
 // Dare l’output relativo.
+
+// creo una funzione
+function palindromeWord(word) {
+
+    // chiedo all'utente di inserire una parola
+    console.log(word);
+
+    // inverto l'ordine delle lettre per leggere la parola al contario
+    // - .split: divide la stringa in lettere e le mette in un array
+    // - .reverse: inverte l'ordine degli elementi dell'array
+    // - .join: unisce gli elementi ell'array in una stringa 
+    let wordReverse = word.split('').reverse().join('')
+    console.log(wordReverse);
+
+    // confronto la parola inserita con quella invertita
+    // se sono uguali sono palindrome, altrimenti no
+    // tampo in pagina il risulato
+    const element = document.getElementById("result");
+    if (word === wordReverse) {
+        element.innerHTML = 'La parola &egrave; palindroma.';
+        console.log('parola palindroma');
+    } else {
+        element.innerHTML = 'La parola non &egrave; palindroma.';
+        console.log('parola non palindroma');
+    }
+}
+
+// richiamo la funzione
+palindromeWord(prompt("Inserisci una parola."));
